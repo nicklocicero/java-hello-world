8edf8fd470fda7a29a16e89dc0a72ee16a9f4738
nicklocicero <nicklocicero@gmail.com>
2018-03-31 17:48:50 -0600

    New class with main method that prints "Hello world!" to system out.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author spy
+ * Simple class that displays the "Hello World!" message. 
+ */
+public class HelloWorld {
+
+  /**
+   * Prints "Hello world!" to standard output.  
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+}
